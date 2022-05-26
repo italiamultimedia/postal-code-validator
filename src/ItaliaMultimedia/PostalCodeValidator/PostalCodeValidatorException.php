dd1216351c634f9a5896f31b1834b173176b6c28
Radu Maier <ariademur@webserv.co>
2022-05-26 16:19:12 +0200

    exception final

--- src/ItaliaMultimedia/PostalCodeValidator/PostalCodeValidatorException.php
+++ src/ItaliaMultimedia/PostalCodeValidator/PostalCodeValidatorException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace ItaliaMultimedia\PostalCodeValidator;
 
-class PostalCodeValidatorException extends \Exception
+final class PostalCodeValidatorException extends \Exception
 {
 }
