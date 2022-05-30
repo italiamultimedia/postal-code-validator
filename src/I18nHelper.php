37df579ec81e8f1e12da644b24920af47dead509
Radu Maier <ariademur@webserv.co>
2022-05-30 11:09:41 +0200

    Update formatting information, make translatable.

---
+++ src/I18nHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+// global code
+//phpcs:ignore SlevomatCodingStandard.Namespaces.NamespaceDeclaration.DisallowedBracketedSyntax, SlevomatCodingStandard.Namespaces.NamespaceSpacing.IncorrectLinesCountBeforeNamespace
+namespace {
+    if (!\function_exists('__')) {
+        /**
+         * Wrapper for "gettext / dgetttext".
+         */
+        function __(string $msg, ?string $domain = null): string
+        {
+            if ($domain) {
+                return \dgettext($domain, $msg);
+            }
+            return \gettext($msg);
+        }
+    }
+
+    if (!\function_exists('___')) {
+        /**
+         * Wrapper for "ngettext / dngettext".
+         */
+        function ___(string $msgSingular, string $msgPlural, int $msgNumber, ?string $domain = null): string
+        {
+            if ($domain) {
+                return \dngettext($domain, $msgSingular, $msgPlural, $msgNumber);
+            }
+            return \ngettext($msgSingular, $msgPlural, $msgNumber);
+        }
+    }
+}
--- src/ItaliaMultimedia/PostalCodeValidator/PostalCodeFormatHelper.php
+++ src/ItaliaMultimedia/PostalCodeValidator/PostalCodeFormatHelper.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace ItaliaMultimedia\PostalCodeValidator;
 
+/**
+* Postal code format information.
+*
+* In order to translate, add to Poedit search path: `vendor/italiamultimedia/postal-code-validator/src`
+*/
 class PostalCodeFormatHelper
 {
     // phpcs:ignore SlevomatCodingStandard.Functions.FunctionLength.FunctionLength,SlevomatCodingStandard.Files.FunctionLength.FunctionLength
@@ -22,17 +27,17 @@ class PostalCodeFormatHelper
             case 'LU':
             case 'NZ':
             case 'SI':
-                return \sprintf('%s numbers', '4');
+                return \__('4 numbers');
             case 'CA':
-                return 'ANA b NAN';
+                return \__('ANA b NAN  (Letter, Number, Letter, a space, Number, Letter, Number: for example M5A 1A2)');
             case 'CN':
             case 'RO':
             case 'RU':
-                return \sprintf('%s numbers', '6');
+                return \__('6 numbers');
             case 'CZ':
-                return '3N b 2N';
+                return \__('3N b 2N (3 numbers, a space, 2 numbers, example: 277 07)');
             case 'GB':
-                return 'AA99 9AA';
+                return \__('last 4 digit must be a space, a numer, 2 letters, example: LS26 8AL');
             case 'DE':
             case 'GR':
             case 'IT':
@@ -47,15 +52,15 @@ class PostalCodeFormatHelper
             case 'SM':
             case 'UA':
             case 'US':
-                return \sprintf('%s numbers', '5');
+                return \__('5 numbers');
             case 'NL':
-                return '4N b 2A';
+                return \__('4N b 2A (4 numbers, a space, 2 letters, example: 1014 DA)');
             case 'PL':
-                return '2n – 3n';
+                return \__('2n – 3n (2 numbers, -, 3 numbers, example: 20-002)');
             case 'PT':
-                return '4n – 3n';
+                return \__('4n – 3n (4 numbers, -, 3 numbers, example: 3000-387)');
             case 'SK':
-                return '3N b 2N';
+                return \__('3N b 2N (3 numbers, a space, 2 numbers, example: 958 01)');
             default:
                 throw new PostalCodeValidatorException(
                     \sprintf('Country not implemented: "%s".', $countryCode),
