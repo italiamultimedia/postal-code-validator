ab7884eca04ec25802113b87ffc2c309369169ec
Radu Maier <ariademur@webserv.co>
2022-07-05 12:32:10 +0200

    CanadaTest

---
+++ tests/unit/ItaliaMultimedia/PostalCodeValidator/CanadaTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\PostalCodeValidator;
+
+use ItaliaMultimedia\PostalCodeValidator\PostalCodeValidator;
+use PHPUnit\Framework\TestCase;
+
+/**
+* @covers \ItaliaMultimedia\PostalCodeValidator\PostalCodeValidator
+*/
+final class CanadaTest extends TestCase
+{
+    protected const COUNTRY_CODE = 'CA';
+
+    public function testAssertInvalidCodeFailSixDigits(): void
+    {
+        $validator = new PostalCodeValidator();
+        self::assertFalse($validator->validate(self::COUNTRY_CODE, '123456'));
+    }
+
+    public function testAssertValidCodePassNiagaraFalls(): void
+    {
+        $validator = new PostalCodeValidator();
+        self::assertTrue($validator->validate(self::COUNTRY_CODE, 'L2J 4L6'));
+    }
+
+    public function testAssertValidCodeFailNiagaraFallsSpaceMissing(): void
+    {
+        $validator = new PostalCodeValidator();
+        self::assertFalse($validator->validate(self::COUNTRY_CODE, 'L2J 4L6'));
+    }
+}
